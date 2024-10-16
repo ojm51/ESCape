c830d2fbdcd5aed35fc87b7f8370477f7031f61a
nayoonju <bubblenana@bubblecon.io>
2024-10-16 14:56:12 +0900

    Feat: 회원가입  post 연결

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
+import { signUp } from "@/libs/axios/auth/auth";
+import { useAuth } from "@/contexts/AuthProvider";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../../public/images/logo.svg";
@@ -18,6 +21,7 @@ export default function SignupPage() {
     mode: "onChange",
   });
   const [loading, setLoading] = useState(false);
+<<<<<<< Updated upstream
 
   const onSubmit = async (data) => {
     setLoading(true); // 로딩 상태 true로 변경
@@ -29,6 +33,23 @@ export default function SignupPage() {
       setLoading(false); // 로딩 상태를 false로 변경
     }, 2000); // 예시로 2초 후에 로딩 종료
     alert("전송버튼클릭");
+=======
+  const router = useRouter();
+  const { login } = useAuth();
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    const { email, password } = data;
+    const isSignUpSuccess = await signUp(data);
+
+    if (isSignUpSuccess) {
+      await login({ email, password });
+      router.push("/");
+      return;
+    }
+    setLoading(false);
+    alert("회원가입 실패");
+>>>>>>> Stashed changes
   };
   const validatePassword = (value) => {
     const regex = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,}$/; // 숫자, 영문, 특수문자 포함 및 최소 8자
@@ -40,7 +61,12 @@ export default function SignupPage() {
 
   const passwordValue = watch("password");
   const [showPassword, setShowPassword] = useState(false);
+<<<<<<< Updated upstream
   const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+=======
+  const [showPasswordConfirmation, setShowPasswordConfirmation] =
+    useState(false);
+>>>>>>> Stashed changes
 
   return (
     <div className="mt-[200px] max-w-[640px] text-white p-3 mx-auto">
@@ -61,7 +87,11 @@ export default function SignupPage() {
             {...register("email", {
               required: "이메일은 필수 입력입니다.",
               pattern: {
+<<<<<<< Updated upstream
                 value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+=======
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+>>>>>>> Stashed changes
                 message: "이메일 형식으로 작성해 주세요.",
               },
             })}
@@ -137,14 +167,24 @@ export default function SignupPage() {
           <label className="block pb-1">비밀번호 확인</label>
           <div className="relative">
             <input
+<<<<<<< Updated upstream
               type={showPasswordConfirm ? "text" : "password"}
               className={`bg-brand-black-medium w-full rounded-xl border-solid ${
                 errors.passwordConfirm
+=======
+              type={showPasswordConfirmation ? "text" : "password"}
+              className={`bg-brand-black-medium w-full rounded-xl border-solid ${
+                errors.passwordConfirmation
+>>>>>>> Stashed changes
                   ? "border-red-500"
                   : "border-brand-black-light"
               } py-4 px-6 text-brand-gray-dark focus:outline-blue-gradation`}
               placeholder="비밀번호를 다시 입력해주세요"
+<<<<<<< Updated upstream
               {...register("passwordConfirm", {
+=======
+              {...register("passwordConfirmation", {
+>>>>>>> Stashed changes
                 required: "비밀번호 확인을 입력해주세요.",
                 validate: (value) =>
                   value === passwordValue || "비밀번호가 일치하지 않습니다.",
@@ -153,25 +193,45 @@ export default function SignupPage() {
             <button
               type="button"
               className="absolute right-5 top-4"
+<<<<<<< Updated upstream
               onClick={() => setShowPasswordConfirm(!showPasswordConfirm)}
               aria-label={
                 showPasswordConfirm ? "비밀번호 숨기기" : "비밀번호 보이기"
+=======
+              onClick={() =>
+                setShowPasswordConfirmation(!showPasswordConfirmation)
+              }
+              aria-label={
+                showPasswordConfirmation ? "비밀번호 숨기기" : "비밀번호 보이기"
+>>>>>>> Stashed changes
               }
             >
               <Image
                 width={25}
+<<<<<<< Updated upstream
                 src={showPasswordConfirm ? EyesShowIcon : EyesHiddenIcon}
                 alt={
                   showPasswordConfirm
+=======
+                src={showPasswordConfirmation ? EyesShowIcon : EyesHiddenIcon}
+                alt={
+                  showPasswordConfirmation
+>>>>>>> Stashed changes
                     ? "비밀번호 숨기기 아이콘"
                     : "비밀번호 보이기 아이콘"
                 }
               />
             </button>
           </div>
+<<<<<<< Updated upstream
           {errors.passwordConfirm && (
             <p className="text-red-500 text-sm mt-2">
               {errors.passwordConfirm.message}
+=======
+          {errors.passwordConfirmation && (
+            <p className="text-red-500 text-sm mt-2">
+              {errors.passwordConfirmation.message}
+>>>>>>> Stashed changes
             </p>
           )}
         </div>
