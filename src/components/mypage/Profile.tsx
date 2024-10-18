86e52056406e4b4e0652e0e55e7ae34ae97187fa
Jeongmin Oh <ojm51@naver.com>
2024-10-18 22:40:50 +0900

    feat: 프로필 컴포넌트 api 연결

--- src/components/mypage/Profile.tsx
+++ src/components/mypage/Profile.tsx
@@ -1,32 +1,40 @@
 import Image from 'next/image'
 import React from 'react'
-import profileImage from '@images/profile_image.png'
+import defaultImage from '@images/default_image.png'
 import PrimaryButton from '../@shared/button/PrimaryButton'
 import TertiaryButton from '../@shared/button/TertiaryButton'
+import { UserTypes } from '@/dtos/UserDto'
 
-export default function Profile() {
+interface ProfileProps {
+  data: UserTypes,
+}
+
+export default function Profile({ data }: ProfileProps) {
   // TertiaryButton을 위한 임시 onClick 함수
   const temp = () => {
     return
   }
 
+  const { image, nickname, description, followersCount, followeesCount } = data;
+  const profileImage = !!image ? image : defaultImage;
+
   return (
     <div className="flex w-full flex-col content-center items-center gap-[30px] rounded-xl border-unactive bg-[#252530] px-5 py-[30px] xl:w-[340px]">
       <Image className="rounded-full" src={profileImage} alt="프로필 이미지" width={120} height={120} />
       <div className="flex w-full flex-col gap-[10px]">
-        <h3 className="text-center text-xl font-semibold leading-7 text-brand-white">User name</h3>
-        <p className="text-sm font-normal leading-tight text-brand-gray-dark">User description</p>
+        <h3 className="text-center text-xl font-semibold leading-7 text-brand-white">{nickname}</h3>
+        <p className="text-sm font-normal leading-tight text-brand-gray-dark">{description}</p>
       </div>
 
       <div className="flex w-full content-between items-center">
-        <div className="flex w-full flex-col content-center items-center gap-[10px] border-r border-r-unactive">
-          <h4 className="text-center text-lg font-semibold text-brand-white">follower</h4>
+        <button className="flex w-full flex-col content-center items-center gap-[10px] border-r border-r-unactive">
+          <h4 className="text-center text-lg font-semibold text-brand-white">{followersCount}</h4>
           <p className="text-center text-sm font-normal text-brand-gray-light">팔로워</p>
-        </div>
-        <div className="flex w-full flex-col content-center items-center gap-[10px]">
-          <h4 className="text-center text-lg font-semibold text-brand-white">following</h4>
+        </button>
+        <button className="flex w-full flex-col content-center items-center gap-[10px]">
+          <h4 className="text-center text-lg font-semibold text-brand-white">{followeesCount}</h4>
           <p className="text-center text-sm font-normal text-brand-gray-light">팔로잉</p>
-        </div>
+        </button>
       </div>
 
       <div className="flex w-full flex-col gap-[10px]">
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,18 +1,53 @@
+import { useQuery } from '@tanstack/react-query'
+import { getMyInfo } from '@/libs/axios/mypage/apis'
+import { Spinner } from 'flowbite-react'
 import Profile from '@/components/mypage/Profile'
-import Activity from '@/components/mypage/Activity'
+import ActivityCardList from '@/components/mypage/ActivityCardList'
 import ThemeCardList from '@/components/mypage/ThemeCardList'
+import Modal from '@/components/@shared/modal/Modal'
+import { UserTypes } from '@/dtos/UserDto'
+
+const defaultUserData: UserTypes = {
+  updatedAt: '',
+  createdAt: '',
+  teamId: '',
+  image: '',
+  description: '',
+  nickname: '',
+  id: 0,
+  mostFavoriteCategory: {
+    name: '',
+    id: 0,
+  },
+  averageRating: 0,
+  reviewCount: 0,
+  followeesCount: 0,
+  followersCount: 0,
+  isFollowing: false,
+};
 
 export default function MyPage() {
+  const { isPending, isError, data } = useQuery({
+    queryKey: ['myInfo'],
+    queryFn: getMyInfo,
+  });
+
+  if(isPending) return <Spinner aria-label='로딩 중...' size='xl' />
+  // if(isError) return <p>failed..</p>
+
   return (
     <>
       <div className="m-auto max-w-[335px] md:max-w-[509px] xl:max-w-[1340px]">
         <section className="mb-[60px] xl:float-left xl:mr-[60px]">
-          <Profile />
+          {/* {!!data ? <Profile data={data} /> : <p>failed..</p>} */}
+          <Profile data={data ?? defaultUserData} />
         </section>
         <section className="mb-[60px]">
-          <Activity />
+          {/* {!!data ? <ActivityCardList data={data} /> : <p>failed..</p>} */}
+          <ActivityCardList data={data ?? defaultUserData} />
         </section>
         <ThemeCardList />
+        {/* <Modal>수리수리마수리님을 팔로우하는 유저</Modal> */}
       </div>
     </>
   )
