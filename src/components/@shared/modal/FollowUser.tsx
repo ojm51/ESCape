85810757263e83d8701a54fef721f7f024acfb70
Jeongmin Oh <ojm51@naver.com>
2024-10-21 12:02:16 +0900

    design: 팔로워 및 팔로잉 모달 내용 구현

---
+++ src/components/@shared/modal/FollowUser.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import { FollowTypes } from '@/dtos/UserDto';
+
+interface FollowUserProps {
+  data: FollowTypes;
+}
+
+export default function FollowUser({ data }: FollowUserProps) {
+  const { image, nickname } = data;
+  return (
+    <button className='flex content-start items-center gap-5'>
+      <Image className='xl:w-[52px]' src={image} alt='프로필 이미지' width={48} height={48} />
+      <p className='text-base font-medium xl:text-lg'>{nickname}</p>
+    </button>
+  )
+}
