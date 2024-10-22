fa9ccd99660d0dcb8813d2dd7af3e8386b699418
Jeongmin Oh <ojm51@naver.com>
2024-10-22 16:13:24 +0900

    feat: useState에 타입 추가

--- src/components/mypage/Profile.tsx
+++ src/components/mypage/Profile.tsx
@@ -7,18 +7,22 @@ import FollowUserList from '../@shared/modal/FollowUserList'
 import CustomButton from '../@shared/button/CustomButton'
 import { FollowListTypes, FollowResponseTypes, UserTypes } from '@/dtos/UserDto'
 import { getUserFollows } from '@/libs/axios/mypage/apis'
+import { useRouter } from 'next/router'
 
 interface ProfileProps {
   data: UserTypes
 }
 
 export default function Profile({ data: userData }: ProfileProps) {
-  const [modalType, setModalType] = useState('팔로워')
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [followData, setFollowData] = useState<FollowResponseTypes>()
+  const [modalType, setModalType] = useState<string>('팔로워')
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   const toggleModal = () => setIsModalOpen((prev) => !prev)
 
-  const { id, image, nickname, description, followersCount, followeesCount } = userData
+  const { id, image, nickname, description, followersCount, followeesCount, isFollowing } = userData
   const profileImage = !!image ? image : defaultImage
+
+  const { pathname } = useRouter()
 
   const {
     isPending: isFollowerPending,
@@ -40,56 +44,76 @@ export default function Profile({ data: userData }: ProfileProps) {
     enabled: !!id,
   })
 
-  const handleButtonClick = (type: string) => {
+  const handleFollowListClick = (type: string) => {
     setModalType(type)
-    if (modalType === '팔로워') {
-      /** @todo 팔로워/팔로잉 리스트 데이터 전달 */
-    }
+    modalType === '팔로워' ? setFollowData(followerList) : setFollowData(followeeList)
     toggleModal()
   }
 
-  // TertiaryButton을 위한 임시 onClick 함수
+  // 로그아웃을 위한 임시 onClick 함수
   const temp = () => {
     return
   }
 
   return (
-    <div className="flex w-full flex-col content-center items-center gap-[30px] rounded-xl border-unactive bg-[#252530] px-5 py-[30px] xl:w-[340px]">
-      <Image className="rounded-full" src={profileImage} alt="프로필 이미지" width={120} height={120} />
-      <div className="flex w-full flex-col gap-[10px]">
-        <h3 className="text-center text-xl font-semibold leading-7 text-brand-white">{nickname}</h3>
-        <p className="text-sm font-normal leading-tight text-brand-gray-dark">{description}</p>
-      </div>
+    <>
+      <div className="flex w-full flex-col content-center items-center gap-[30px] rounded-xl border-unactive bg-[#252530] px-5 py-[30px] xl:w-[340px]">
+        <Image className="rounded-full" src={profileImage} alt="프로필 이미지" width={120} height={120} />
+        <div className="flex w-full flex-col gap-[10px]">
+          <h3 className="text-center text-xl font-semibold leading-7 text-brand-white">{nickname}</h3>
+          <p className="text-sm font-normal leading-tight text-brand-gray-dark">{description}</p>
+        </div>
 
-      <div className="flex w-full content-between items-center">
-        <button
-          className="flex w-full flex-col content-center items-center gap-[10px]"
-          onClick={() => handleButtonClick('팔로워')}
-        >
-          <h4 className="text-center text-lg font-semibold text-brand-white">{followersCount}</h4>
-          <p className="text-center text-sm font-normal text-brand-gray-light">팔로워</p>
-        </button>
-        <button
-          className="flex w-full flex-col content-center items-center gap-[10px]"
-          onClick={() => handleButtonClick('팔로잉')}
-        >
-          <h4 className="text-center text-lg font-semibold text-brand-white">{followeesCount}</h4>
-          <p className="text-center text-sm font-normal text-brand-gray-light">팔로잉</p>
-        </button>
-      </div>
+        <div className="flex w-full content-between items-center">
+          <button
+            className="flex w-full flex-col content-center items-center gap-[10px]"
+            onClick={() => handleFollowListClick('팔로워')}
+          >
+            <h4 className="text-center text-lg font-semibold text-brand-white">{followersCount}</h4>
+            <p className="text-center text-sm font-normal text-brand-gray-light">팔로워</p>
+          </button>
+          <button
+            className="flex w-full flex-col content-center items-center gap-[10px]"
+            onClick={() => handleFollowListClick('팔로잉')}
+          >
+            <h4 className="text-center text-lg font-semibold text-brand-white">{followeesCount}</h4>
+            <p className="text-center text-sm font-normal text-brand-gray-light">팔로잉</p>
+          </button>
+        </div>
 
-      <div className="flex w-full flex-col gap-[10px]">
-        <CustomButton active={true}>프로필 편집</CustomButton>
-        <CustomButton style="tertiary" active={true} onClick={temp}>
-          로그아웃
-        </CustomButton>
+        {/** @todo
+         * accessToken이 있으면 -> 프로필 편집 & 로그아웃
+         * accessToken이 없으면 ->
+         *  팔로우 중일 땐 '팔로우 취소'
+         *  팔로우 중이 아닐 땐 '팔로우'*/}
+        {pathname === 'mypage' ? (
+          <div className="flex w-full flex-col gap-[10px]">
+            <CustomButton active={true}>프로필 편집</CustomButton>
+            <CustomButton style="tertiary" active={true} onClick={temp}>
+              로그아웃
+            </CustomButton>
+          </div>
+        ) : isFollowing ? (
+          <CustomButton style="tertiary" active={true}>
+            팔로우 취소
+          </CustomButton>
+        ) : (
+          <CustomButton active={true}>팔로우</CustomButton>
+        )}
       </div>
 
       {isModalOpen && (
-        <Modal onClick={toggleModal}>
-          <FollowUserList name={nickname} title={`${modalType === '팔로워' ? '을 팔로우' : '이 팔로잉'}`} />
+        <Modal
+          onClick={toggleModal}
+          classNames="max-h-[550px] w-[335px] overflow-auto scrollbar-hide md:max-h-[600px] md:w-[500px] xl:max-h-[660px]"
+        >
+          <FollowUserList
+            name={nickname}
+            title={`${modalType === '팔로워' ? '을 팔로우' : '이 팔로잉'}`}
+            followData={followData?.list}
+          />
         </Modal>
       )}
-    </div>
+    </>
   )
 }
