b5149fb0f571c3a4e0f628af5dc7c217b69e66be
Jeongmin Oh <ojm51@naver.com>
2024-10-22 16:14:09 +0900

    design: 유저 프로필 화면 생성

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,7 +1,54 @@
+import { useQuery } from '@tanstack/react-query'
+import { getMyInfo } from '@/libs/axios/mypage/apis'
+import { Spinner } from 'flowbite-react'
+import Profile from '@/components/mypage/Profile'
+import ActivityCardList from '@/components/mypage/ActivityCardList'
+import ThemeCardList from '@/components/mypage/ThemeCardList'
+import { UserTypes } from '@/dtos/UserDto'
+import { useRouter } from 'next/router'
+
+const defaultUserData: UserTypes = {
+  updatedAt: '',
+  createdAt: '',
+  teamId: '',
+  image: '',
+  description: '',
+  nickname: '',
+  id: 0,
+  mostFavoriteCategory: {
+    name: '',
+    id: 0,
+  },
+  averageRating: 0,
+  reviewCount: 0,
+  followeesCount: 0,
+  followersCount: 0,
+  isFollowing: false,
+}
+
 export default function UserPage() {
+  const { pathname } = useRouter()
+  const { isPending, isError, data } = useQuery({
+    queryKey: [`userInfo${pathname}`],
+    queryFn: getMyInfo,
+  })
+
+  if (isPending) return <Spinner aria-label="로딩 중..." size="xl" />
+  // if(isError) return <p>failed..</p>
+
   return (
     <>
-      <div>유저 프로필 페이지</div>
+      <div className="m-auto max-w-[335px] md:max-w-[509px] xl:max-w-[1340px]">
+        <section className="mb-[60px] xl:float-left xl:mr-[60px]">
+          {/* {!!data ? <Profile data={data} /> : <p>failed..</p>} */}
+          <Profile data={data ?? defaultUserData} />
+        </section>
+        <section className="mb-[60px]">
+          {/* {!!data ? <ActivityCardList data={data} /> : <p>failed..</p>} */}
+          <ActivityCardList data={data ?? defaultUserData} />
+        </section>
+        <ThemeCardList data={data ?? defaultUserData} />
+      </div>
     </>
   )
 }
