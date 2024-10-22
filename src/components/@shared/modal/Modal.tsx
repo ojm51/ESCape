c30d3865064dbd8585bf052a8c10f8ecd02778b1
Jeongmin Oh <ojm51@naver.com>
2024-10-22 16:12:22 +0900

    design: 공통 모달 스타일 수정

--- src/components/@shared/modal/Modal.tsx
+++ src/components/@shared/modal/Modal.tsx
@@ -1,19 +1,20 @@
-import React, { ComponentPropsWithoutRef, useState } from 'react'
+import React, { ComponentPropsWithoutRef } from 'react'
 import Image from 'next/image'
 import closeIcon from '@icons/close_icon.svg'
 
 interface ModalProps extends ComponentPropsWithoutRef<'div'> {
   onClick: () => void
+  classNames?: string
 }
 
-export default function Modal({ children, onClick }: ModalProps) {
+export default function Modal({ children, onClick, classNames }: ModalProps) {
   return (
     <div
       className="fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm"
       onClick={onClick}
     >
       <section
-        className="relative max-h-[550px] w-[335px] overflow-auto rounded-xl bg-[#1b1b22] px-5 py-10 scrollbar-hide md:max-h-[600px] md:w-[500px] md:py-[60px] xl:max-h-[660px] xl:rounded-2xl"
+        className={`${classNames} relative rounded-xl bg-[#1b1b22] px-5 py-10 md:px-10 md:py-[60px] xl:rounded-2xl`}
         onClick={(e) => e.stopPropagation()}
       >
         <button className="absolute right-4 top-4 ml-auto md:right-5 md:top-5 md:w-9 xl:w-10" onClick={onClick}>
