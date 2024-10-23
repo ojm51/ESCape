4b8d2b06a5db5a4d16deee0780e87cd04aceeee4
Jeongmin Oh <ojm51@naver.com>
2024-10-23 23:00:20 +0900

    feat: 내 프로필 업데이트 함수 생성

--- src/libs/axios/mypage/apis.ts
+++ src/libs/axios/mypage/apis.ts
@@ -1,7 +1,7 @@
 import { FollowResponseTypes, UserTypes } from '@/dtos/UserDto'
 import axiosInstance from '../axiosInstance'
 import { API_PATH } from '../config/path'
-import { getUserFollowsParams, getUserInfoParams, getUserProductsParams } from './types'
+import { GetUserFollowsParams, GetUserInfoParams, GetUserProductsParams, UpdateMyInfoParams } from './types'
 import { ProductListTypes } from '@/dtos/ProductDto'
 
 export const getMyInfo = async () => {
@@ -9,18 +9,24 @@ export const getMyInfo = async () => {
   return response.data ?? []
 }
 
-export const getUserInfo = async ({ userId }: getUserInfoParams) => {
+export const updateMyInfo = async ({ description, nickname, image }: UpdateMyInfoParams) => {
+  const params = { description, nickname, image }
+  const response = await axiosInstance.patch<UserTypes>(API_PATH.user.me, params)
+  return response.data ?? []
+}
+
+export const getUserInfo = async ({ userId }: GetUserInfoParams) => {
   const response = await axiosInstance.get<UserTypes>(API_PATH.user.user(userId))
   return response.data ?? []
 }
 
-export const getUserProducts = async ({ userId, type }: getUserProductsParams) => {
+export const getUserProducts = async ({ userId, type }: GetUserProductsParams) => {
   const url = type === 'reviewed' ? API_PATH.user.reviewedProducts(userId) : API_PATH.user.favoriteProducts(userId)
   const response = await axiosInstance.get<ProductListTypes>(url)
   return response.data.list ?? []
 }
 
-export const getUserFollows = async ({ userId, type }: getUserFollowsParams) => {
+export const getUserFollows = async ({ userId, type }: GetUserFollowsParams) => {
   const url = type === '팔로워' ? API_PATH.user.followers(userId) : API_PATH.user.followees(userId)
   const response = await axiosInstance.get<FollowResponseTypes>(url)
   return response.data ?? []
--- src/libs/axios/mypage/types.ts
+++ src/libs/axios/mypage/types.ts
@@ -1,13 +1,19 @@
-export interface getUserInfoParams {
+export interface UpdateMyInfoParams {
+  description: string
+  nickname: string
+  image: string
+}
+
+export interface GetUserInfoParams {
   userId: string | number
 }
 
-export interface getUserProductsParams {
+export interface GetUserProductsParams {
   userId: string | number
   type: string
 }
 
-export interface getUserFollowsParams {
+export interface GetUserFollowsParams {
   userId: string | number
   type: string
 }
