8db00be513dd830a0e348d39e36b938d05f1327a
nayoonju <bubblenana@bubblecon.io>
2024-10-24 16:26:22 +0900

    fix: 구글 외부 이미지 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com'], // 허용할 도메인 추가
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com', 'lh3.googleusercontent.com'],
   },
 }
 
