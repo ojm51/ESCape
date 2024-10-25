79e706c7dfe7051a196a922ed96bed3210b9e9ae
Jeongmin Oh <ojm51@naver.com>
2024-10-25 23:07:51 +0900

    feat: 이미지 업로드 api 함수 추가

--- src/libs/axios/mypage/apis.ts
+++ src/libs/axios/mypage/apis.ts
@@ -1,16 +1,23 @@
 import { FollowResponseTypes, UserTypes } from '@/dtos/UserDto'
 import axiosInstance from '../axiosInstance'
 import { API_PATH } from '../config/path'
-import { GetUserFollowsParams, GetUserInfoParams, GetUserProductsParams, UpdateMyInfoParams } from './types'
+import {
+  AddImageFileParams,
+  GetUserFollowsParams,
+  GetUserInfoParams,
+  GetUserProductsParams,
+  UpdateMyInfoParams,
+} from './types'
 import { ProductListTypes } from '@/dtos/ProductDto'
+import { ImageTypes } from '@/dtos/ImageDto'
 
 export const getMyInfo = async () => {
   const response = await axiosInstance.get<UserTypes>(API_PATH.user.me)
   return response.data ?? []
 }
 
-export const updateMyInfo = async ({ description, nickname, image }: UpdateMyInfoParams) => {
-  const params = { description, nickname, image }
+export const updateMyInfo = async ({ image, nickname, description }: UpdateMyInfoParams) => {
+  const params = { image, nickname, description }
   const response = await axiosInstance.patch<UserTypes>(API_PATH.user.me, params)
   return response.data ?? []
 }
@@ -30,4 +37,16 @@ export const getUserFollows = async ({ userId, type }: GetUserFollowsParams) => 
   const url = type === '팔로워' ? API_PATH.user.followers(userId) : API_PATH.user.followees(userId)
   const response = await axiosInstance.get<FollowResponseTypes>(url)
   return response.data ?? []
+}
+
+export const addImageFile = async ({ image }: AddImageFileParams) => {
+  const formData = new FormData()
+  formData.append('image', image)
+
+  const response = await axiosInstance.post<ImageTypes>(API_PATH.image.default, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+  return response.data.url ?? []
 }
