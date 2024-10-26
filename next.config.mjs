5bc10ae77a6c97bf4726bbb82a6ab8b9a59f8939
Jeongmin Oh <ojm51@naver.com>
2024-10-26 12:20:03 +0900

    chore: next.config 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      'lh3.googleusercontent.com',
+      'k.kakaocdn.net',
+      'search.pstatic.net',
+    ],
+  },
 }
 
 export default nextConfig
