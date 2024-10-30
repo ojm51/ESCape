166877b1bef2056b0163e278eda3a3bb63395d89
kimsayhi <kimse418@gmail.com>
2024-10-30 14:42:11 +0900

    Fix: 린트에러 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import { useAuth } from '@/contexts/AuthProvider'
-import { useState, useEffect } from 'react'
-
 export default function HomePage() {
   return <h2 className="p-10 text-center text-white">홈 화면입니다.</h2>
 }
