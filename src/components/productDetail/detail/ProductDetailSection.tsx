acdc6cf31ae8e00f1556ed221887987fcf30b287
SealBros <hoon980923@gmail.com>
2024-10-31 00:52:12 +0900

    feat:로딩처리 작업

--- src/components/productDetail/detail/ProductDetailSection.tsx
+++ src/components/productDetail/detail/ProductDetailSection.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQueryClient } from '@tanstack/react-query'
 import Image from 'next/image'
 import { IoMdShare } from 'react-icons/io'
 import { useAuth } from '@/contexts/AuthProvider'
 import { useToaster } from '@/contexts/ToasterProvider'
-import { fetchProductDetails } from '@/libs/axios/product/productApi'
 import { ProductDetailTypes, DescriptionTypes } from '@/dtos/ProductDto'
 import FeatureTag from './FeatureTag'
 import CustomButton from '../../@shared/ui/CustomButton'
@@ -12,22 +11,16 @@ import FavoriteButton from './FavoriteButton'
 import KakaoShareButton from './KakaoShareButton'
 import ReviewModal from '../ReviewModal'
 
-const ProductDetailSection: React.FC<{ productId: number }> = ({ productId }) => {
+const ProductDetailSection: React.FC<{ productId: number; detailData: ProductDetailTypes }> = ({
+  productId,
+  detailData,
+}) => {
   const queryClient = useQueryClient()
-  const { data, isLoading, error, refetch } = useQuery<ProductDetailTypes>({
-    queryKey: ['productDetail', productId],
-    queryFn: () => fetchProductDetails(productId),
-  })
-
   const { user } = useAuth()
   const toaster = useToaster()
   const [isModalOpen, setModalOpen] = useState(false)
 
-  if (isLoading) return <div>로딩 중...</div>
-  if (error) return <div>제품 세부 정보를 불러오지 못했습니다.</div>
-
-  const productDetail = data as ProductDetailTypes
-  const parsedDescription = productDetail?.description as DescriptionTypes
+  const parsedDescription = detailData?.description as DescriptionTypes
 
   const handleShareClick = () => {
     const shareUrl = window.location.href
@@ -56,17 +49,17 @@ const ProductDetailSection: React.FC<{ productId: number }> = ({ productId }) =>
   }
 
   const handleRefetch = () => {
-    refetch()
+    queryClient.invalidateQueries({ queryKey: ['productDetail', productId] })
   }
 
   return (
     <div className="mx-auto flex max-w-[940px] flex-col rounded-md pt-6 text-white lg:flex-row">
       <div className="mb-4 flex-shrink-0 lg:mb-0 lg:mr-6">
         <Image
-          src={productDetail.image}
+          src={detailData.image}
           width={128}
           height={128}
-          alt={productDetail.name}
+          alt={detailData.name}
           className="h-48 w-48 rounded-md object-fill"
         />
       </div>
@@ -74,15 +67,15 @@ const ProductDetailSection: React.FC<{ productId: number }> = ({ productId }) =>
       <div className="flex w-full flex-col justify-between">
         <div className="mb-2 flex items-center justify-between">
           <div className="flex items-center">
-            <h2 className="text-2xl font-bold">{productDetail.name}</h2>
-            <FavoriteButton productId={productId} isFavorite={productDetail.isFavorite} onRefetch={handleRefetch} />
+            <h2 className="text-2xl font-bold">{detailData.name}</h2>
+            <FavoriteButton productId={productId} isFavorite={detailData.isFavorite} onRefetch={handleRefetch} />
           </div>
           <div className="flex space-x-4">
             <KakaoShareButton
               url={window.location.href}
-              title={productDetail.name}
+              title={detailData.name}
               description={parsedDescription.des}
-              imageUrl={productDetail.image}
+              imageUrl={detailData.image}
             />
             <button
               type="button"
@@ -124,9 +117,8 @@ const ProductDetailSection: React.FC<{ productId: number }> = ({ productId }) =>
           onClose={() => {
             setModalOpen(false)
             queryClient.invalidateQueries({ queryKey: ['reviews', productId] })
-            refetch()
           }}
-          productName={productDetail.name}
+          productName={detailData.name}
           productId={productId}
         />
       )}
--- src/components/productDetail/review/ProductReviewSection.tsx
+++ src/components/productDetail/review/ProductReviewSection.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQueryClient } from '@tanstack/react-query'
 import { useRouter } from 'next/router'
 import { useAuth } from '@/contexts/AuthProvider'
 import { useToaster } from '@/contexts/ToasterProvider'
-import { fetchReviews, deleteReview } from '@/libs/axios/product/reviewApi'
+import { deleteReview } from '@/libs/axios/product/reviewApi'
 import { ProductReviewListTypes } from '@/dtos/ProductDto'
 import DefaultImage from '@images/default-image.png'
 import SortDropdown from './SortDropdown'
@@ -11,7 +11,13 @@ import StarRating from '../StarRating'
 import ReviewLikeButton from './ReviewLikeButton'
 import ReviewModal from '../ReviewModal'
 
-const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) => {
+interface ProductReviewSectionProps {
+  productId: number
+  reviews: ProductReviewListTypes[]
+  refetchReviews: () => void
+}
+
+const ProductReviewSection: React.FC<ProductReviewSectionProps> = ({ productId, reviews, refetchReviews }) => {
   const router = useRouter()
   const toaster = useToaster()
   const { user } = useAuth()
@@ -20,19 +26,9 @@ const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) =>
   const [isReviewModalOpen, setIsReviewModalOpen] = useState<boolean>(false)
   const [editingReview, setEditingReview] = useState<ProductReviewListTypes | null>(null)
 
-  const {
-    data: reviews = [],
-    isLoading,
-    error,
-    refetch,
-  } = useQuery<ProductReviewListTypes[]>({
-    queryKey: ['reviews', productId, sortOption],
-    queryFn: () => fetchReviews(productId, sortOption),
-  })
-
   const handleSortChange = (newSortOption: string) => {
     setSortOption(newSortOption)
-    refetch()
+    refetchReviews() // 정렬 옵션 변경 시 데이터 다시 가져오기
   }
 
   const handleProfileClick = (userId: number) => {
@@ -51,7 +47,7 @@ const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) =>
       try {
         await deleteReview(reviewId)
         toaster('success', '리뷰가 성공적으로 삭제되었습니다.')
-        refetch()
+        refetchReviews()
         queryClient.invalidateQueries({ queryKey: ['productDetail', productId] })
       } catch (error) {
         console.error('리뷰 삭제 실패:', error)
@@ -63,7 +59,7 @@ const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) =>
   const closeModal = () => {
     setIsReviewModalOpen(false)
     setEditingReview(null)
-    refetch()
+    refetchReviews()
     queryClient.invalidateQueries({ queryKey: ['productDetail', productId] })
   }
 
@@ -73,26 +69,18 @@ const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) =>
         <h3 className="text-lg font-semibold">상품 리뷰</h3>
         <SortDropdown order={handleSortChange} />
       </div>
-      {isLoading ? (
-        <p>로딩 중...</p>
-      ) : error ? (
-        <p>{(error as Error).message}</p>
+      {reviews.length === 0 ? (
+        <p>리뷰가 없습니다.</p>
       ) : (
         <ul>
           {reviews.map(review => (
             <li key={review.id} className="relative mb-4 rounded-lg border border-unactive bg-[#252530] p-6">
               <div className="md:flex md:justify-between">
-                {/* 모바일에서는 section이 위에, 그 아래에 리뷰 내용이 배치되도록 설정 */}
                 <section className="mb-4 md:mb-0">
                   <div className="flex items-start">
                     <button
                       type="button"
                       onClick={() => handleProfileClick(review.user.id)}
-                      onKeyDown={e => {
-                        if (e.key === 'Enter' || e.key === ' ') {
-                          handleProfileClick(review.user.id)
-                        }
-                      }}
                       className="mr-4 h-12 w-12 cursor-pointer overflow-hidden rounded-full object-cover focus:outline-none"
                       aria-label={`${review.user.nickname}의 프로필`}
                     >
@@ -114,7 +102,6 @@ const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) =>
                   </div>
                 </section>
 
-                {/* 모바일에서 아래로 배치되도록 설정 */}
                 <div className="space-y-2 md:mx-8 md:flex md:flex-grow md:flex-col">
                   <p className="text-white">{review.content}</p>
                   <div className="flex space-x-2">
@@ -132,7 +119,6 @@ const ProductReviewSection: React.FC<{ productId: number }> = ({ productId }) =>
                   </div>
 
                   <div className="flex items-center justify-between">
-                    {/* 작성 시간과 수정/삭제 버튼을 옆에 배치 */}
                     <div className="flex items-center space-x-2 text-gray-400">
                       <p>{new Date(review.createdAt).toLocaleDateString()}</p>
                       {user && user.id === review.user.id && (
